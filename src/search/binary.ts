4d746eec8fa44d15894ef2e6b185d68b2e7b95b3
千寻 <qianxun@qianxundeMacBook-Air.local>
2021-10-16 01:33:29 +0800

    feat: code changes

--- src/search/binary.ts
+++ src/search/binary.ts
@@ -1,3 +1,9 @@
+/**
+ * 二分查找，返回等于目标最左边下标
+ * @param data 有序数据
+ * @param target 查找目标
+ * @returns 目标最左下标
+ */
 export function binary<T>(data: T[], target: T): number {
   let len = data.length;
   if (len === 0 || (len === 1 && data[0] !== target)) {
